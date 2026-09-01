551edbda98cce99b7bed11ac1d3ef7876c245366
dkchw <dangkchuong@gmail.com>
2026-09-01 18:20:57 +0000

    Auto-release v3.27

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aidict.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 65
-        versionName = "3.26"
+        versionCode = 66
+        versionName = "3.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
